93ad483c7b66c4eb3e24155d8b742ea3c7315744
omersenem <omerrsenem@gmail.com>
2023-02-19 15:34:26 +0300

    axios with get request complete

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Users></Users>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import Users from "./components/UsersPage.vue"
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+
+
+
 </script>
 
 <style>
---
+++ src/components/UsersPage.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <table class="table">
+      <thead>
+      <tr>
+        <th scope="col">Id</th>
+        <th scope="col">Name</th>
+        <th scope="col">Email</th>
+        <th scope="col">Website</th>
+        <th scope="col">Compony</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="user in users" :key="user.id">
+        <th scope="row">{{user.id}}</th>
+        <td>{{user.name}}</td>
+        <td>{{user.email}}</td>
+        <td>{{user.website}}</td>
+        <td>{{user.company.name}}</td>
+      </tr>
+
+
+      </tbody>
+    </table>
+  </div>
+
+</template>
+<script setup>
+
+import {onMounted, ref} from "vue";
+import axios from "axios";
+
+const users = ref(null);
+onMounted(()=>{
+  axios.get("https://jsonplaceholder.typicode.com/users").then( (response) => {
+users.value = response.data;
+        console.log(users.value)
+      }
+  );
+
+})
+
+</script>
